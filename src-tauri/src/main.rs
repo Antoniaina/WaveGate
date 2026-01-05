139569fe3ff3ebae748ea4759cd997071f843fd9
RAMAHALISON Antoniaina <arisehenoantoniaina@gmail.com>
2026-01-06 01:36:34 +0300

    feat: add tray icon to toggle popup window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{
+    Manager,
+    tray::{TrayIconBuilder, TrayIconEvent},
+};
+
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let tray = TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click { .. } = event {
+                        let app = tray.app_handle();
+                        let window = app.get_webview_window("popup").unwrap();
+
+                        if window.is_visible().unwrap() {
+                            window.hide().unwrap();
+                        } else {
+                            window.show().unwrap();
+                            window.set_focus().unwrap();
+                        }
+                    }
+                })
+                .build(app)?;
+
+            app.manage(tray);
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running EqControl");
 }
