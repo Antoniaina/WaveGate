6317af0b4138abf7287872615790991cf0e1f7a6
RAMAHALISON Antoniaina <arisehenoantoniaina@gmail.com>
2026-01-06 22:21:39 +0300

    feat: toggle popup window on tray icon click

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri::{
-    Manager,
-    tray::{TrayIconBuilder, TrayIconEvent},
-    WindowEvent
+    Manager, tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent}
 };
 
 fn main() {
@@ -13,16 +11,16 @@ fn main() {
             let tray = TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
                 .on_tray_icon_event(|tray, event| {
-                    if let TrayIconEvent::Click { .. } = event {
+                    if let TrayIconEvent::Click { button: MouseButton::Left, button_state: MouseButtonState::Up, .. } = event {
                         let app = tray.app_handle();
-                        let window = app.get_webview_window("popup").unwrap();
-
-                        if window.is_visible().unwrap() {
-                            window.hide().unwrap();
-                        } else {
-                            window.show().unwrap();
-                            window.set_focus().unwrap();
-                        }
+                        if let Some(window) = app.get_webview_window("popup") {
+                            if window.is_visible().unwrap_or(false) {
+                                let _ = window.hide();
+                            } else {
+                                let _ = window.show();
+                                let _ = window.set_focus();
+                            }
+                        }                       
                     }
                 })
                 .build(app)?;
